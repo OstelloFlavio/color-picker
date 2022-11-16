8f34b05767fcde822bdb050d60bf0208182efe1d
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-16 13:15:59 +0100

    Aggiunta JS e fix HTML/CSS

---
+++ static/JS/index.js
@@ -0,0 +1,47 @@
+function rosso(bottone)
+{
+    let oldBottone = document.getElementById("pulsante_premuto");
+    oldBottone.id = 'pulsante';
+    document.body.style.backgroundColor="#FF0000";
+    bottone.id ='pulsante_premuto'
+}
+
+function cyan(bottone)
+{
+    let oldBottone = document.getElementById("pulsante_premuto");
+    oldBottone.id = 'pulsante';
+    document.body.style.backgroundColor="#00FFFF";
+    bottone.id ='pulsante_premuto'
+}
+
+function blue(bottone)
+{
+    let oldBottone = document.getElementById("pulsante_premuto");
+    oldBottone.id = 'pulsante';
+    document.body.style.backgroundColor="#0000FF";
+    bottone.id ='pulsante_premuto'
+}
+
+function green(bottone)
+{
+    let oldBottone = document.getElementById("pulsante_premuto");
+    oldBottone.id = 'pulsante';
+    document.body.style.backgroundColor="#008000";
+    bottone.id ='pulsante_premuto'
+}
+
+function purple(bottone)
+{
+    let oldBottone = document.getElementById("pulsante_premuto");
+    oldBottone.id = 'pulsante';
+    document.body.style.backgroundColor="#800080";
+    bottone.id ='pulsante_premuto'
+}
+
+function yellow(bottone)
+{
+    let oldBottone = document.getElementById("pulsante_premuto");
+    oldBottone.id = 'pulsante';
+    document.body.style.backgroundColor="#FFFF00";
+    bottone.id ='pulsante_premuto'
+}
